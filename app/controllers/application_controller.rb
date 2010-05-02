d6e16463a910c9fdee79da2914bf246e4254e963
Eric Sipple <saalon@gmail.com>
2010-05-02 16:02:53 -0400

    Copied typo's method of handling themes, but this method can probably be cleaned up.  But it is working.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  before_filter :set_theme
+
+  def set_theme
+    self.view_paths = ::ActionController::Base.view_paths.dup.unshift("#{RAILS_ROOT}/themes/default/views")
+  end
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password

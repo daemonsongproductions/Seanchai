5b43feba512fea2690f020317457c932a7932ae5
Eric Sipple <saalon@gmail.com>
2010-05-02 21:40:50 -0400

    Created a route to an admin index.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :stories
 
+  map.admin 
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:

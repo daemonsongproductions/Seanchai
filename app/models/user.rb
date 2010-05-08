78012a6ec207aa226d3454bca35c414a84aa8fa0
Eric Sipple <saalon@gmail.com>
2010-05-08 12:59:25 -0400

    Began work on user model, primarily on password encryption and salting.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,29 @@
 class User < ActiveRecord::Base
 
-  
+  require 'digest/sha1'
+
+  validates_length_of :email, :within => 6..40
+  validates_length_of :password, :within => 5..40
+  validates_presence_of :email, :password, :password_salt
+  validates_uniqueness_of :email
+  validates_format_of :email, :with => /^(\w+?@\w+?\x2E.+)$/i, :message => "Your login must be an email address."
+
+  attr_protected :id, :password_salt, :password, :user_type_id
+
+  def set_password(password)
+    self.password_salt =  generate_random_string unless self.password_salt?
+    self.password = User.encrypt_password(password, self.password_salt)
+  end
+
+  def self.encrypt_password(password, salt)
+    Digest::SHA1.hexdigest(password + salt).to_s
+  end
+
+  def generate_random_string(length = 10)
+    chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+    random_string = ""
+    1.upto(length) { |i| random_string << chars[rand(chars.size-1)] }
+    random_string
+  end
 
 end

a79c1ada0f2611b97c621516ac3260c8f4022e71
Eric Sipple <saalon@gmail.com>
2010-05-09 15:41:27 -0400

    Got rspec working, added in a couple of tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,24 @@ require 'spec_helper'
 
 describe User do
   before(:each) do
-    @valid_attributes = {
+    @protected_attributes = { :password => 'password',
+                              :password_salt => 'salt',
+                              :user_type_id  => '1'
       
     }
   end
 
-  it "should create a new instance given valid attributes" do
-    User.create!(@valid_attributes)
+  it "should not allow password, password_salt or user_type_id to be set via mass assignment" do
+    user = User.new(@protected_attributes)
+    user.password.should be nil
+    user.password_salt.should be nil
+    user.user_type_id.should be nil
   end
+
+  it "should set the password_salt when the password is set" do
+    user = User.new
+    user.password = 'password'
+    user.password_salt.should_not be nil
+  end
+
 end

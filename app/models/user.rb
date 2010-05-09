95a78e21d26bc5700dffdcfa030da74d82fe42c0
Eric Sipple <saalon@gmail.com>
2010-05-09 15:11:30 -0400

    Overrode the password_salt method to ensure it's being called as part of a password change before allowing it to go through.  There is probably a cleaner implementation of this.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,9 @@ class User < ActiveRecord::Base
   attr_protected :id, :password_salt, :password, :user_type_id
 
   def password=(password)
-    self.password_salt =  generate_random_string unless self.password_salt?
-    self.password = User.encrypt_password(password, self.password_salt)
+    @password_change_request = true
+    self.password_salt = generate_random_string unless self.password_salt?
+    super(User.encrypt_password(password, self.password_salt))
   end
 
   def self.encrypt_password(password, salt)
@@ -24,6 +25,12 @@ class User < ActiveRecord::Base
     random_string = ""
     1.upto(length) { |i| random_string << chars[rand(chars.size-1)] }
     random_string
+  end
+
+  def password_salt=(password_salt)
+    raise "The password salt can only be changed as part of a password change." unless @password_change_request
+    super password_salt
+    @password_change_request = false
   end
 
 end

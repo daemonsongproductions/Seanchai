2426cb943c51bd000daf4d2ebdef4abf96a31db4
Eric Sipple <saalon@gmail.com>
2010-05-09 18:57:08 -0400

    Added some more tests for the User model and set a default for user_type_id that is absolutely not correct.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,18 @@ class User < ActiveRecord::Base
 
   validates_length_of :email, :within => 6..40
   validates_length_of :password, :within => 5..40
-  validates_presence_of :email, :password, :password_salt
+  validates_presence_of :email, :password, :password_salt, :user_type_id
   validates_uniqueness_of :email
   validates_format_of :email, :with => /^(\w+?@\w+?\x2E.+)$/i, :message => "Your login must be an email address."
 
   attr_protected :id, :password_salt, :password, :user_type_id
+
+
+  def save
+    self.user_type_id = 1 unless self.user_type_id?
+    super  
+  end
+
 
   def password=(password)
     @password_change_request = true
@@ -16,21 +23,27 @@ class User < ActiveRecord::Base
     super(User.encrypt_password(password, self.password_salt))
   end
 
-  def self.encrypt_password(password, salt)
-    Digest::SHA1.hexdigest(password + salt).to_s
+  def is_password?(password)
+    self.password == User.encrypt_password(password, self.password_salt)
   end
 
-  def generate_random_string(length = 10)
-    chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
-    random_string = ""
-    1.upto(length) { |i| random_string << chars[rand(chars.size-1)] }
-    random_string
+  def self.encrypt_password(password, salt)
+    Digest::SHA1.hexdigest(password + salt).to_s
   end
 
   def password_salt=(password_salt)
     raise "The password salt can only be changed as part of a password change." unless @password_change_request
     super password_salt
     @password_change_request = false
+  end
+
+  private
+
+  def generate_random_string(length = 10)
+    chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+    random_string = ""
+    1.upto(length) { |i| random_string << chars[rand(chars.size-1)] }
+    random_string
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,23 @@ require 'spec_helper'
 
 describe User do
   before(:each) do
-    @protected_attributes = { :password => 'password',
-                              :password_salt => 'salt',
-                              :user_type_id  => '1'
-      
+
+  end
+
+  def protected_attributes
+    { :password => 'password',
+      :password_salt => 'salt',
+      :user_type_id  => '1'
+
     }
   end
 
+  def correct_password
+    "password"
+  end
+
   it "should not allow password, password_salt or user_type_id to be set via mass assignment" do
-    user = User.new(@protected_attributes)
+    user = User.new(protected_attributes)
     user.password.should be nil
     user.password_salt.should be nil
     user.user_type_id.should be nil
@@ -21,5 +29,38 @@ describe User do
     user.password = 'password'
     user.password_salt.should_not be nil
   end
+
+  it 'should encrypt and salt the password' do
+    user = User.new
+    user.password = correct_password
+    user.password.should_not match correct_password
+    user.password.should_not match User.encrypt_password(correct_password, '')
+    user.password.should match User.encrypt_password(correct_password, user.password_salt)
+  end
+
+  describe "Is Password Correct" do
+
+    def incorrect_password
+      "invalid"
+    end
+
+    def user_with_password
+      user = User.new
+      user.password = correct_password
+      user
+    end
+
+    it "should return true if the supplied password matches the saved password" do
+      user = user_with_password
+      user.is_password?(correct_password).should be true
+    end
+
+    it "should return false if the supplied password does not match the saved password" do
+      user = user_with_password
+      user.is_password?(incorrect_password).should be false      
+    end
+
+  end
+
 
 end

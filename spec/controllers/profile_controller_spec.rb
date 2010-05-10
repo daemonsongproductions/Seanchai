2d4d2e1286658185b0561271685d8c10c0f6a619
Eric Sipple <saalon@gmail.com>
2010-05-10 13:27:10 -0400

    Added a simple test for the create method of the profile controller.

--- spec/controllers/profile_controller_spec.rb
+++ spec/controllers/profile_controller_spec.rb
@@ -2,5 +2,21 @@ require 'spec_helper'
 
 describe ProfileController do
 
+  before :each do
+    @params = { :email => 'email@email.com', :password => 'password'}
+  end
+
+  it "should add a new user with the supplied parameters" do
+
+    user = User.new
+    User.stub(:new).and_return(user)
+    User.stub(:save).and_return(true)
+    post :create, @params
+    
+    user.email.should match @params[:email]
+    user.password.should match User.encrypt_password(@params[:password], user.password_salt)
+
+  end
+
 
 end

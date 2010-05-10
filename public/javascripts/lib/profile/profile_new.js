6d2214a90c48e68b3025ad6d34f640179a6f4f63
Eric Sipple <saalon@gmail.com>
2010-05-10 13:04:44 -0400

    Moved validation rules into the profile_new.js file.

--- public/javascripts/lib/profile/profile_new.js
+++ public/javascripts/lib/profile/profile_new.js
@@ -1,5 +1,27 @@
+var validation_rules  = {
+    rules :{
+        "email": {required: true, email: true },
+        "password": {required: true, minlength: 6},
+        "confirm_password": {required: true, equalTo: "#password" }
+    },
+    messages :{
+        "email": {
+            required: "Email address is required",
+            email:"Email address format is invalid"
+        },
+        "password": {
+            required: "Password is required.",
+            minlength: "Password must be at least six characters long"
+        },
+        "confirm_password": {
+            required: "Password is required.",
+            equalTo: "Passwords must match."
+        }
+    }
+};
+
 $(document).ready(function(){
-    $("#registration_form").validate();
+    $("#registration_form").validate(validation_rules);
 
     $('#submit_button').click(function() {
         $('#registration_form').valid();

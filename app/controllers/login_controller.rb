809dcb4488112be56b1401219cd05b598cb94036
Eric Sipple <saalon@gmail.com>
2010-05-11 23:20:15 -0400

    Created a login controller.

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,3 @@
+class LoginController < ApplicationController
+
+end
---
+++ spec/controllers/login_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe LoginController do
+
+  it "should email a new password when requested for the supplied email"
+
+  it "should redirect to the confirmation page even when supplied with a nonexistant email"
+
+end

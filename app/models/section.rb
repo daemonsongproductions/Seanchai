60bbc3331ccf3b974269fdd7a5eb7719f72183d4
Eric Sipple <saalon@gmail.com>
2011-01-24 13:02:38 -0500

    Linking story and related models.

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,2 +1,5 @@
 class Section < ActiveRecord::Base
+  belongs_to :story
+  belongs_to :section_type
+  has_many :chapters
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,5 @@
 class Story < ActiveRecord::Base
   belongs_to :user
+  has_many :sections
+  has_many :chapters
 end

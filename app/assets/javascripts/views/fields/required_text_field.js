9b1a5c714191ed709ce8f5d4a76338b711037d91
Eric Sipple <saalon@gmail.com>
2013-09-04 07:47:18 -0400

    Working on a validation. Still broken.

--- app/assets/javascripts/views/fields/required_text_field.js
+++ app/assets/javascripts/views/fields/required_text_field.js
@@ -2,6 +2,6 @@ Seanchai.RequiredTextField = Ember.View.extend({
   classNames:['control-group'],
   templateName: "fields/required_text_field",
   isValid: function(){
-
-  }
+    return (!inputElement || 0 === inputElement.length)
+  }.property("inputElement.elementId")
 });

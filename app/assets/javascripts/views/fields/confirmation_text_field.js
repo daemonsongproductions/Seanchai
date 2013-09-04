4ba5d11417062737741afc315e2e7e136b7a03f4
Eric Sipple <saalon@gmail.com>
2013-09-04 17:46:09 -0400

    Refactored confirm field.
    Broke the missing and different conditionals into their own methods. Gave a little aura around the indput boxes that are required.

--- app/assets/javascripts/views/fields/confirmation_text_field.js
+++ app/assets/javascripts/views/fields/confirmation_text_field.js
@@ -3,6 +3,13 @@ Seanchai.ConfirmationTextField = Ember.View.extend({
   templateName: "fields/confirmation_text_field",
   fieldType: "text",
   invalid: function(){
-    return ((this.confirmField.value !== this.value) || (0 === this.value.length));
-  }.property("value")
+    return ( this.missing(this) || this.different(this));
+  }.property("value", "confirmField.value"),
+  missing: function(view) {
+    return (0 === view.value.length);
+  },
+  different: function(view) {
+    debugger;
+    return (view.confirmField.value !== view.value);
+}
 });

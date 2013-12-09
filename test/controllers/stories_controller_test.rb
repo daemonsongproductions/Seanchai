c92f243c89442063814981c914048e0bf2b100dd
Eric Sipple <sipple@trustarts.org>
2013-12-09 13:21:58 -0500

    Adding controller test shells.

---
+++ test/controllers/stories_controller_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class StoriesControllerTest < MiniTest::Unit::TestCase
+  include Mongoid::Matchers
+
+  def test_sanity
+    skip("leaving this as an example of unit test syntax in minitest")
+  end
+
+end
+
+describe "StoriesController" do
+
+  describe "show" do
+    it "should have needed fields" do
+      skip("Pending")
+    end
+  end
+
+end

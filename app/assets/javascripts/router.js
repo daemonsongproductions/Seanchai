899db0700b2c4be78213d0cb5d579db59189c3f5
Eric Sipple <sipple@trustarts.org>
2013-12-26 15:00:57 -0500

    Added edit story path to ember router.

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -12,6 +12,7 @@ Seanchai.Router.map(function() {
 
   this.resource('stories', function(){
     this.route('new');
+    this.resource('edit_story', {path: ':story_id/edit'})
   });
   this.resource('story', { path: '/story/:story_id' });
 

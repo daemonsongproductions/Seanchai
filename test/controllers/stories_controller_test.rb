af498ddd111a745cabd6db34ca4f81c098551d77
Eric Sipple <sipple@trustarts.org>
2014-01-02 14:51:39 -0500

    Removing useless test.

--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -1,14 +1,5 @@
 require "test_helper"
 
-class StoriesControllerTest < MiniTest::Unit::TestCase
-  include Mongoid::Matchers
-
-  def test_sanity
-    skip("leaving this as an example of unit test syntax in minitest")
-  end
-
-end
-
 describe "StoriesController" do
 
   describe "index" do

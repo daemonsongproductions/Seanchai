2ccfba3a378b24693ba699b7a9b3dd99e873976b
Eric Sipple <saalon@gmail.com>
2014-01-09 10:53:04 -0500

    Just adding missing semicolons. Nothing to see here.

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -6,20 +6,20 @@ Seanchai.Router.map(function() {
   this.route("registration");
 
   this.resource('users', function() {
-    this.resource('user', {path: ':username'})
-    this.resource('edit_user', {path: ':username/edit'})
-    this.resource('user_stories', {path: ':username/stories'})
+    this.resource('user', {path: ':username'});
+    this.resource('edit_user', {path: ':username/edit'});
+    this.resource('user_stories', {path: ':username/stories'});
   });
 
   this.resource('stories', function(){
     this.route('new');
-    this.resource('stories_edit', {path: ':story_id/edit'})
+    this.resource('stories_edit', {path: ':story_id/edit'});
   });
   this.resource('story', { path: '/story/:story_id' }, function() {
     this.resource('story_sections_index', {path: ''});
     this.resource('story_sections_new', {path: 'story_sections/new'});
   });
-  this.resource('story_section_show', {path: '/story/:story_id/:story_section_id'})
+  this.resource('story_section_show', {path: '/story/:story_id/:story_section_id'});
 
 });
 

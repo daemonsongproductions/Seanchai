44df8a83759d3cc098aa4946d0c70dba0cd0227e
Eric Sipple <saalon@gmail.com>
2014-01-10 13:04:11 -0500

    Removing include_in_toc from ember model until I need to use it.

--- app/assets/javascripts/models/story_section.js
+++ app/assets/javascripts/models/story_section.js
@@ -1,7 +1,7 @@
 Seanchai.StorySection = DS.Model.extend({
   title: DS.attr('string'),
   body: DS.attr('string'),
-  include_in_toc: DS.attr('string'),
+  //include_in_toc: DS.attr('string'),
   //status_id: DS.attr('number'),
   story_id: DS.attr('string'),
   editable: DS.attr('boolean'),

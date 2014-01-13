c50131c873ba8be33d0493e33e3bbce19351b43b
Eric Sipple <saalon@gmail.com>
2014-01-13 11:35:27 -0500

    Stopping work here, and am possibly moving away from Mongodb. Crapballs.

--- app/assets/javascripts/routes/story_sections_routes/story_sections_show_route.js
+++ app/assets/javascripts/routes/story_sections_routes/story_sections_show_route.js
@@ -1,11 +1,5 @@
 Seanchai.StorySectionsShowRoute = Ember.Route.extend({
   model: function(params) {
-    var the_story = this.modelFor('story');
-    var route = this;
-    this.get('store').find('story', params.story_id).then(function(answer){
-      var story = route.get('store').createRecord('story', answer);
-      return story.get('story_sections').find(params.id);
-    });
-
+    return this.get('store').find('story_section', params.id);
   }
 });

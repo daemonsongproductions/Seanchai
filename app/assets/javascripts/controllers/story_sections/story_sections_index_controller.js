5cc72be92383fc33859395b1337e1809ab61e489
Eric Sipple <saalon@gmail.com>
2014-01-13 15:58:35 -0500

    Show technically working. Tests need to be fixed and some form of partial record return implemented.

--- app/assets/javascripts/controllers/story_sections/story_sections_index_controller.js
+++ app/assets/javascripts/controllers/story_sections/story_sections_index_controller.js
@@ -2,5 +2,10 @@ Seanchai.StorySectionsIndexController = Ember.ArrayController.extend({
   sortProperties: ['title'],
   sortAscending: true,
   needs: "story",
-  story: Ember.computed.alias("controllers.story")
+  story: Ember.computed.alias("controllers.story"),
+  actions: {
+    load_section: function(section) {
+      this.transitionToRoute('story_sections_index', section);
+    }
+  }
 });
--- app/assets/javascripts/controllers/story_sections/story_sections_show_controller.js
+++ app/assets/javascripts/controllers/story_sections/story_sections_show_controller.js
@@ -1,10 +1,5 @@
 Seanchai.StorySectionsIndexController = Ember.ArrayController.extend({
   sortProperties: ['title'],
   sortAscending: true,
-  needs: "story",
-  section: function() {
-    return this.find(function(section) {
-      return (section.get('id') == "this");
-    });
-  }.property('model')
+  needs: "story"
 });

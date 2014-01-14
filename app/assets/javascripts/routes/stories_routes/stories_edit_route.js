dac7ee358a31ffb6b2b35d95af0784ea15ffcb40
Eric Sipple <saalon@gmail.com>
2014-01-14 11:39:54 -0500

    Rolling back model when edit cancelled.

--- app/assets/javascripts/routes/stories_routes/stories_edit_route.js
+++ app/assets/javascripts/routes/stories_routes/stories_edit_route.js
@@ -2,5 +2,18 @@ Seanchai.StoriesEditRoute = Ember.Route.extend({
   templateName: "stories/edit",
   model: function(params) {
     return this.get('store').find('story', params.story_id);
+  },
+  actions: {
+    willTransition: function(transition) {
+      controller = this.controllerFor('story');
+      if (controller.get('isDirty') &&
+          !confirm("Are you sure you want to abandon progress?")) {
+        transition.abort();
+        return false;
+      } else {
+        this.get('currentModel').rollback();
+        return true;
+      }
+    }
   }
 });

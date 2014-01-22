9288514fcc84d21417159fb567c461e628623098
Eric Sipple <saalon@gmail.com>
2014-01-22 15:40:52 -0500

    Working towards a better find for story sections.

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,6 +1,8 @@
 Seanchai.Store = DS.Store.extend({
   find_section: function(story_slug, order) {
-    return this.find('story_section', order + "?story_id=" + story_slug)
+    return this.find('story_section', order + "?story_id=" + story_slug);
+    //var adapter = this.adapterFor('story_section')
+    //return adapter.ajax(adapter.buildURL('story_section', order), 'GET', {data: {story_id: story_slug}});
   }
 });
 

5d4cd887fbc9a94a443c06893d38743d2745126d
Eric Sipple <saalon@gmail.com>
2014-01-24 13:28:16 -0500

    Getting deployment working for staging.

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,5 +54,27 @@ namespace :deploy do
       # end
     end
   end
+end
 
+desc "Check that we can access everything"
+task :check_write_permissions do
+  on roles(:all) do |host|
+    if test("[ -w #{fetch(:deploy_to)} ]")
+      info "#{fetch(:deploy_to)} is writable on #{host}"
+    else
+      error "#{fetch(:deploy_to)} is not writable on #{host}"
+    end
+  end
+end
+
+# lib/capistrano/tasks/agent_forwarding.cap
+desc "Check if agent forwarding is working"
+task :forwarding do
+  on roles(:all) do |h|
+    if test("env | grep SSH_AUTH_SOCK")
+      info "Agent forwarding is up to #{h}"
+    else
+      error "Agent forwarding is NOT up to #{h}"
+    end
+  end
 end
